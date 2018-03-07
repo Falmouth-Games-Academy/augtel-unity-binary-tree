9fbe9be9e86c300d569c94ef29552e286c79cf59
Developer <Developer@018TGAL-60175.staff.fal.ac.uk>
2018-03-07 10:56:12 +0000

    first commit

---
+++ Assets/Scripts/BinaryTree.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryTree : MonoBehaviour {
+
+    public GameObject treeNode;
+    private GameObject rootNode;
+    private TreeNode rootNodeScript;
+
+    private Vector2 mousePos;
+    private Vector3 screenPos;
+    public Camera camera;
+    public float speed;
+
+    private int degs = 0;
+
+	// Use this for initialization
+	void Start () {
+        camera = Camera.main;
+        initRoot();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("up"))
+        {
+            rootNode.transform.Translate(Vector3.right * 1.0f);
+            //rootNode.transform.localPosition += new Vector3(1, 0, 0);
+
+            rootNodeScript = rootNode.GetComponent<TreeNode>();
+            if (rootNodeScript != null)
+            {
+                //rootNodeScript.updatePosition(new Vector3(0, 1, 0));
+            }
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            rootNodeScript = rootNode.GetComponent<TreeNode>();
+            if (rootNodeScript != null)
+            {
+                degs += 1;
+                //rootNodeScript.updateRotation(rootNode.transform.position, new Vector3(-3f, 0, 0));
+            }
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            rootNodeScript = rootNode.GetComponent<TreeNode>();
+            if (rootNodeScript != null)
+            {
+                degs -= 1;
+                //rootNodeScript.updateRotation(rootNode.transform.position, new Vector3(3f, 0, 0));
+            }
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Debug.Log("DOT");
+        }
+
+        if (Input.GetKeyUp(KeyCode.M))
+        {
+            Debug.Log("DASH");
+        }
+
+        
+        
+
+
+    }
+
+    void initRoot()
+    {
+
+        rootNode = Instantiate(treeNode);
+        TreeNode.setParent(rootNode);
+
+        rootNode.name = "Root";
+        rootNode.transform.SetParent(transform);
+
+        Vector3 pos = transform.position;
+        pos.z += 29;
+        pos.y += 0;
+        pos.x += 12;
+        transform.position = pos;
+
+        if (rootNode != null)
+        {
+            rootNodeScript = rootNode.GetComponent<TreeNode>();
+            if (rootNodeScript != null)
+            {
+
+                rootNodeScript.setValue('*');
+                rootNodeScript.setIndex(32);
+                
+            }
+        }
+
+        initTree();
+    }
+
+    void initTree()
+    {
+        rootNodeScript.insert(16,   'E');
+       rootNodeScript.insert(48,   'T');
+
+       
+      rootNodeScript.insert(8,    'I');
+      rootNodeScript.insert(24,   'A');
+      rootNodeScript.insert(40,   'N');
+      rootNodeScript.insert(56,   'M');
+
+        
+      rootNodeScript.insert(4,    'S');
+      rootNodeScript.insert(12,   'U');
+      rootNodeScript.insert(20,   'R');
+      rootNodeScript.insert(28,   'W');
+      rootNodeScript.insert(36,   'D');
+      rootNodeScript.insert(44,   'K');
+      rootNodeScript.insert(52,   'G');
+      rootNodeScript.insert(60,   'O');
+        
+      rootNodeScript.insert(2,    'H');
+      rootNodeScript.insert(6,    'V');
+      rootNodeScript.insert(10,   'F');
+      rootNodeScript.insert(14,   'Ü');
+      rootNodeScript.insert(18,   'L');
+      rootNodeScript.insert(22,   'Ä');
+      rootNodeScript.insert(26,   'P');
+      rootNodeScript.insert(30,   'J');
+      rootNodeScript.insert(34,   'B');
+      rootNodeScript.insert(38,   'X');
+      rootNodeScript.insert(42,   'C');
+      rootNodeScript.insert(46,   'Y');
+      rootNodeScript.insert(50,   'Z');
+      rootNodeScript.insert(54,   'Q');
+      rootNodeScript.insert(58,   'Ö');
+      rootNodeScript.insert(62,   '-');
+
+      rootNodeScript.insert(1,    '5');
+      rootNodeScript.insert(3,    '4');
+      rootNodeScript.insert(5,    'Ś');
+      rootNodeScript.insert(7,    '3');
+      rootNodeScript.insert(9,    'É');
+      rootNodeScript.insert(11,   '-');
+      rootNodeScript.insert(13,   'Ð');
+      rootNodeScript.insert(15,   '2');
+      rootNodeScript.insert(17,   '&');
+      rootNodeScript.insert(19,   'É');
+      rootNodeScript.insert(21,   '+');
+      rootNodeScript.insert(23,   '-');
+      rootNodeScript.insert(25,   '-');
+      rootNodeScript.insert(27,   'À');
+      rootNodeScript.insert(29,   'ĵ');
+      rootNodeScript.insert(31,   '1');
+      rootNodeScript.insert(33,    '6');
+      rootNodeScript.insert(35,   '=');
+      rootNodeScript.insert(37,   '/');
+      rootNodeScript.insert(39,   '-');
+      rootNodeScript.insert(41,   'Ç');
+      rootNodeScript.insert(43,   '-');
+      rootNodeScript.insert(45,   '(');
+      rootNodeScript.insert(47,   '-');
+      rootNodeScript.insert(49,    '7');
+      rootNodeScript.insert(51,   '-');
+      rootNodeScript.insert(53,   'Ğ');
+      rootNodeScript.insert(55,   'Ñ');
+      rootNodeScript.insert(57,    '8');
+      rootNodeScript.insert(59,   '-');
+      rootNodeScript.insert(61,   '9');
+      rootNodeScript.insert(63,   '0');
+      
+
+    }
+}
---
+++ Assets/Scripts/TreeNode.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TreeNode : MonoBehaviour {
+
+    public static GameObject parent;
+    public GameObject rightOrb;
+    public GameObject leftOrb;
+
+    public GameObject smallTreeNodePrefab;
+
+    public char value = '*';
+    public int index = 0;
+    public int depth = 0;
+
+    // dispay component for char
+    public TextMeshPro orbText;
+    
+    // LEFT child node instance vars
+    GameObject leftNode;
+    GameObject leftJoin;
+    Vector3 leftNodePosition;
+    Vector3 leftStartPosition;
+    Vector3 leftEndPosition;
+
+    // RIGHT child node instance vars
+    GameObject rightNode;
+    GameObject rightJoin;
+    Vector3 rightNodePosition;
+    Vector3 rightStartPosition;
+    Vector3 rightEndPosition;
+
+    // positioning of child nodes
+    private float horizontalOffset = 20;
+    private int verticalOffset = -7;
+
+    
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void insert (int i, char v, int d = 0)
+    {
+        // step depth
+        d++;
+
+        // check against this value
+        if (i < index)
+        {
+            if (leftNode == null)
+            {
+                if (depth == 4)
+                {
+                    leftNode = Instantiate(smallTreeNodePrefab, parent.transform, true) as GameObject;// , transform, false);
+                    
+                }
+                else
+                {
+                    leftNode = Instantiate(leftOrb, parent.transform, true) as GameObject; //b, transform, false);
+                    Debug.Log(v);
+                }
+                leftNode.name = "left-node-" + v;
+                setChildValues(leftNode, i, v, d);
+                positionLeftNode();
+                
+            }
+            else
+            {   
+                insertToChild(leftNode, i, v, d);
+            }
+        }
+        else if (i > index) { 
+            
+            if (rightNode == null){
+
+                if (depth == 4)
+                {
+                    rightNode = Instantiate(smallTreeNodePrefab, parent.transform, true) as GameObject;
+                }
+                else
+                {
+                    
+                    rightNode = Instantiate(rightOrb, parent.transform, true) as GameObject;
+                    
+                }
+
+                rightNode.name = "right-node-" + v;
+                setChildValues(rightNode, i, v, d);
+                positionRightNode();
+            }
+            else
+            {
+               
+                insertToChild(rightNode, i, v, d);
+                
+            }
+
+            
+        }
+    }
+
+    public void setValue (char v)
+    {
+        value = v;
+        orbText.SetText("" + v);
+    }
+
+    public void setIndex (int i)
+    {
+        index = i;
+    }
+
+   
+
+    public void setDepth (int d)
+    {
+        depth = d;
+        horizontalOffset = (Mathf.Round((80/(Mathf.Pow(2,d+2f))) * 100.0f) / 100f);
+    }
+
+    private void setChildValues(GameObject node, int i, char v, int d)
+    {
+        if (node != null)
+        {
+            var nodeScript = node.GetComponent<TreeNode>();
+            if (nodeScript != null)
+            {
+                nodeScript.setValue(v);
+                nodeScript.setIndex(i);
+                nodeScript.setDepth(d);
+                
+            }
+        }
+    }
+
+    private void insertToChild(GameObject node, int i, char v, int d)
+    {
+        if (node != null)
+        {
+            var nodeScript = node.GetComponent<TreeNode>();
+            if (nodeScript != null)
+            {
+                nodeScript.insert(i, v, d);
+            }
+        }
+
+    }
+
+    private void positionLeftNode()
+    {
+       
+
+        leftNode.transform.position = transform.position;
+        leftNode.transform.position += new Vector3(-horizontalOffset, verticalOffset, 0);
+        
+        
+        // create the left join positions
+        leftStartPosition = Vector3.MoveTowards(transform.position, leftNode.transform.position, 1.3f);
+        leftEndPosition = Vector3.MoveTowards(leftNode.transform.position, transform.position, 1.3f);
+
+
+
+        leftJoin = new GameObject("leftJoin");
+        leftJoin.transform.SetParent(parent.transform, true);
+        LineRenderer leftLine = leftJoin.AddComponent<LineRenderer>();
+
+        leftLine.material = new Material(Shader.Find("Sprites/Default"));
+        leftLine.widthMultiplier = 0.3f;
+        leftLine.positionCount = 2;
+        leftLine.useWorldSpace = false;
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(new Color(0.2F, 0.3F, 0.4F, 0.5F), 0.0f), new GradientColorKey(new Color(0.5F, 0.8F, 0.9F, 0.5F), 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+
+        leftLine.colorGradient = gradient;
+
+        leftLine.SetPosition(0, leftStartPosition);
+        leftLine.SetPosition(1, leftEndPosition);
+        
+    }
+
+    private void positionRightNode()
+    {
+       
+        rightNode.transform.position = transform.position;
+        rightNode.transform.position += new Vector3(horizontalOffset, verticalOffset, 0);
+
+        
+        // create the left join positions
+        rightStartPosition = Vector3.MoveTowards(transform.position, rightNode.transform.position, 1.3f);
+        rightEndPosition = Vector3.MoveTowards(rightNode.transform.position, transform.position, 1.3f);
+
+        
+        rightJoin = new GameObject("rightJoin");
+        rightJoin.transform.SetParent(parent.transform, true);
+        LineRenderer rightLine = rightJoin.AddComponent<LineRenderer>();
+
+        rightLine.material = new Material(Shader.Find("Sprites/Default"));
+        rightLine.widthMultiplier = 0.3f;
+        rightLine.positionCount = 2;
+        rightLine.useWorldSpace = false;
+
+        // Create a gradient
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(new Color(0.2F, 0.3F, 0.4F, 0.5F), 0.0f), new GradientColorKey(new Color(0.5F, 0.8F, 0.9F, 0.5F), 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+
+        rightLine.colorGradient = gradient;
+
+        rightLine.SetPosition(0, rightStartPosition);
+        rightLine.SetPosition(1, rightEndPosition);
+        
+    }
+
+    public static void setParent(GameObject p)
+    {
+        parent = p;
+    }
+    
+    
+
+}
